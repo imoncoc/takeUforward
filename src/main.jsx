a73dcbf88c7c7b7265d7cd5b16124a495e1078c1
Md Imon Hossain <imoncoc0@gmail.com>
2023-04-09 22:57:52 +0600

    tailwindCss, toaster, heroIcon initial setup added

--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,25 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { BeakerIcon } from "@heroicons/react/24/solid";
+import { Toaster } from "react-hot-toast";
 
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: (
+      <div className="text-2xl underline text-gray-800 font-sans">
+        Hello world!
+        <BeakerIcon className="h-6 w-6 text-blue-500" />
+      </div>
+    ),
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Toaster></Toaster>
     <RouterProvider router={router} />
   </React.StrictMode>
 );

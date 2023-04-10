957134413b652ad3c7c12eb5257b198be3cc6c3e
Md Imon Hossain <imoncoc0@gmail.com>
2023-04-10 11:31:21 +0600

    hero section added, error page bug fixed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import menImg  from '../../assets/allImg/men.png'
+import './Home.css'
 
 const Home = () => {
     return (
-      <div>
-        <h2>Home Page</h2>
+      <div className="container">
+        <div className="row d-flex flex-row-reverse">
+          <div className="col-10 col-md-6 mx-auto">
+            <img className="img-fluid" src={menImg} alt="" />
+          </div>
+          <div
+            className="col-10 col-md-6 mx-auto 
+          align-self-center pe-5"
+          >
+            <div className="mt-5">
+              <h2 className="dream-home-title fs-1 fw-bolder">
+                One Step <br /> Closer To Your <br />
+                <span className="active">Dream Job</span>
+              </h2>
+              <p className='dream-home-description'>
+                Explore thousands of job opportunities with all the information
+                you need. Its your future. Come find it. Manage all your job
+                application from start to finish.
+              </p>
+              <button className="dream-btn-primary">Get Started</button>
+            </div>
+          </div>
+        </div>
       </div>
     );
 };

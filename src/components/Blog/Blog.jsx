dc5a2d78cf873d2f52bbc506882eb8bfcc96e79c
Md Imon Hossain <imoncoc0@gmail.com>
2023-04-11 15:49:14 +0600

    bug fixed on blog page

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -30,11 +30,11 @@ const Blog = () => {
         <div className="container">
           <div className="row">
             <div className="col-10 col-sm-12 mx-auto my-5">
-              <div class="accordion" id="accordionExample">
-                <div class="accordion-item">
-                  <h2 class="accordion-header">
+              <div className="accordion" id="accordionExample">
+                <div className="accordion-item">
+                  <h2 className="accordion-header">
                     <button
-                      class="accordion-button"
+                      className="accordion-button"
                       type="button"
                       data-bs-toggle="collapse"
                       data-bs-target="#collapseOne"
@@ -46,10 +46,10 @@ const Blog = () => {
                   </h2>
                   <div
                     id="collapseOne"
-                    class="accordion-collapse collapse show"
+                    className="accordion-collapse collapse show"
                     data-bs-parent="#accordionExample"
                   >
-                    <div class="accordion-body">
+                    <div className="accordion-body">
                       <strong className='active'>Contest API: </strong> Context is primarily used
                       when some data needs to be accessible by many components
                       at different nesting levels. Apply it sparingly because it
@@ -59,10 +59,10 @@ const Blog = () => {
                     </div>
                   </div>
                 </div>
-                <div class="accordion-item">
-                  <h2 class="accordion-header">
+                <div className="accordion-item">
+                  <h2 className="accordion-header">
                     <button
-                      class="accordion-button collapsed"
+                      className="accordion-button collapsed"
                       type="button"
                       data-bs-toggle="collapse"
                       data-bs-target="#collapseTwo"
@@ -74,10 +74,10 @@ const Blog = () => {
                   </h2>
                   <div
                     id="collapseTwo"
-                    class="accordion-collapse collapse"
+                    className="accordion-collapse collapse"
                     data-bs-parent="#accordionExample"
                   >
-                    <div class="accordion-body">
+                    <div className="accordion-body">
                       <strong className='active'>Custom Hook: </strong> Custom React JS hooks are
                       reusable functions that a React JS software developer can
                       use to add special and unique functionality to the React
@@ -87,10 +87,10 @@ const Blog = () => {
                     </div>
                   </div>
                 </div>
-                <div class="accordion-item">
-                  <h2 class="accordion-header">
+                <div className="accordion-item">
+                  <h2 className="accordion-header">
                     <button
-                      class="accordion-button collapsed"
+                      className="accordion-button collapsed"
                       type="button"
                       data-bs-toggle="collapse"
                       data-bs-target="#collapseThree"
@@ -102,10 +102,10 @@ const Blog = () => {
                   </h2>
                   <div
                     id="collapseThree"
-                    class="accordion-collapse collapse"
+                    className="accordion-collapse collapse"
                     data-bs-parent="#accordionExample"
                   >
-                    <div class="accordion-body">
+                    <div className="accordion-body">
                       <strong className='active'>useRef:</strong> useRef is a built-in React hook
                       that accepts one argument as the initial value and returns
                       a reference . A reference is an object having a special
@@ -113,10 +113,10 @@ const Blog = () => {
                     </div>
                   </div>
                 </div>
-                <div class="accordion-item">
-                  <h2 class="accordion-header">
+                <div className="accordion-item">
+                  <h2 className="accordion-header">
                     <button
-                      class="accordion-button collapsed"
+                      className="accordion-button collapsed"
                       type="button"
                       data-bs-toggle="collapse"
                       data-bs-target="#collapseFour"
@@ -128,10 +128,10 @@ const Blog = () => {
                   </h2>
                   <div
                     id="collapseFour"
-                    class="accordion-collapse collapse"
+                    className="accordion-collapse collapse"
                     data-bs-parent="#accordionExample"
                   >
-                    <div class="accordion-body">
+                    <div className="accordion-body">
                       <strong className='active'>useMemo:</strong> React has a built-in hook called
                       useMemo that allows you to memoize expensive functions so
                       that you can avoid calling them on every render. You
